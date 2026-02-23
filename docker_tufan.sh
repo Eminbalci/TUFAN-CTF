a7b632addbad053aa4aaac3a9d6a92509fd43ab0
Muhammet Emin Balcıoğlu <50174379+Eminbalci@users.noreply.github.com>
2026-02-23 11:47:10 +0300

    Load Docker image and run container with permissions

--- docker_tufan.sh
+++ docker_tufan.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# İmajı yükle
 sudo docker load -i talon_ros_yedek.tar
 
+# Ekran izni ver (Grafik arayüzü için)
 xhost +local:docker
 
+# Konteyneri çalıştır
 sudo docker run -it \
     --name ardupilot_yeni \
     --user dev \
     --network host \
     --gpus all \
     --device /dev/dri:/dev/dri \
+    --device /dev/video0:/dev/video0 \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/home/dev/.Xauthority \
